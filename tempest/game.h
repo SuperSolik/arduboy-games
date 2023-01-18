9c94736199141ec715b54346962c6adee346901e
SuperSolik <serpetr99@gmail.com>
2023-01-18 19:01:35 +0100

    init boilerplate code

---
+++ tempest/game.h
@@ -0,0 +1,34 @@
+#include <Arduboy2.h>
+
+class Game {
+  static constexpr uint8_t DEFAULT_FPS = 24;
+
+ public:
+  Game(const Arduboy2& arduboy, uint8_t fps = DEFAULT_FPS) {
+    this->arduboy = arduboy;
+    this->fps = fps;
+  }
+
+  void init() {
+    this->arduboy.begin();
+    this->arduboy.setFrameRate(this->fps);
+    this->arduboy.initRandomSeed();
+    this->reset();
+  }
+
+  void run() {
+    if (!this->arduboy.nextFrame()) return;
+    this->arduboy.clear();
+    this->arduboy.pollButtons();
+
+    this->arduboy.print("hello world");
+
+    this->arduboy.display();
+  }
+
+ private:
+  void reset() {}
+
+  Arduboy2 arduboy;
+  uint8_t fps;
+};
---
+++ tempest/tempest.ino
@@ -0,0 +1,15 @@
+#include <Arduboy2.h>
+
+#include "game.h"
+
+Arduboy2 a;
+
+Game game(a);
+
+void setup () {
+  game.init();
+}
+
+void loop () {
+  game.run();
+}
