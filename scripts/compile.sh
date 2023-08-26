343fba2a13e27e9efba8c095d19e85c0855e4df5
SuperSolik <serpetr99@gmail.com>
2023-08-26 23:20:24 +0200

    added --build-cache-path to compile script

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,4 +2,4 @@
 
 set -xe 
 
-rm -rf build/* && arduino-cli compile -b arduino:avr:leonardo --build-path ./build/ $1
+rm -rf build/* && arduino-cli compile -b arduino:avr:leonardo --build-cache-path ./build/ --build-path ./build/ $1
