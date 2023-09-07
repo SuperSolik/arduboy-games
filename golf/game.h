67cba40bee2e85b24e0989f40aa7353c16283df6
SuperSolik <serpetr99@gmail.com>
2023-09-08 00:50:02 +0200

    starting kick mechanic

--- golf/game.h
+++ golf/game.h
@@ -4,6 +4,8 @@
 constexpr int SUBPIXELS = 16;
 constexpr int SUB_WIDTH = WIDTH * SUBPIXELS;
 constexpr int SUB_HEIGHT = HEIGHT * SUBPIXELS;
+constexpr int VEL_DECREASE_FACTOR = 2;
+constexpr int INIT_VEL = 45;
 
 #define TO_WORLD(x) (int)(x >> 4)
 
@@ -21,17 +23,36 @@ struct Ball {
 
   int vel_x;
   int vel_y;
+  bool is_moving;
 
   void init() {
     x = SUB_WIDTH / 2;
     y = SUB_HEIGHT / 2;
     radius = 4;
-    vel_x = 16;
-    vel_y = 16;
+    vel_x = 0;
+    vel_y = 0;
+    is_moving = false;
   }
 
   void draw(Arduboy2& a) {
     a.fillCircle(TO_WORLD(x), TO_WORLD(y), this->radius, WHITE);
+  }
+
+  void update(const float& angle) {
+      this->x -= (int)(this->vel_x * cosf(angle));
+      this->y -= (int)(this->vel_y * sinf(angle));
+      // todo: decrease velocity based in current animation frame
+      this->vel_x -= VEL_DECREASE_FACTOR;
+      if (this->vel_x <= 0) {
+        this->vel_x = 0;
+      }
+
+      this->vel_y -= VEL_DECREASE_FACTOR;
+      if (this->vel_y <= 0) {
+        this->vel_y = 0;
+      } 
+      
+      this->is_moving = (this->vel_x != 0) || (this->vel_y != 0);
   }
 };
 
@@ -99,6 +120,7 @@ class Game {
   }
 
   void game_loop() {
+    // draw
     if (this->arduboy.pressed(LEFT_BUTTON)) {
       this->stick.angle += M_PI / 90;
       if (this->stick.angle >= 2 * M_PI) {
@@ -113,16 +135,20 @@ class Game {
       }
     }
 
-    if (this->arduboy.pressed(A_BUTTON)) {
-      this->ball.x -= (int)(this->ball.vel_x * cosf(this->stick.angle));
-      this->ball.y -= (int)(this->ball.vel_y * sinf(this->stick.angle));
+    if (this->arduboy.pressed(A_BUTTON) && !this->ball.is_moving) {
+      this->ball.vel_x = INIT_VEL;
+      this->ball.vel_y = INIT_VEL;
     }
 
+    this->ball.update(this->stick.angle);
+
+
+    // draw
     this->arduboy.print(TO_WORLD(ball.x));
     this->arduboy.print("\n");
     this->arduboy.print(TO_WORLD(ball.y));
     this->arduboy.print("\n");
-    this->arduboy.print(stick.angle * 180 / M_PI);
+    this->arduboy.print(this->ball.is_moving);
 
     this->ball.draw(this->arduboy);
     this->stick.draw(this->arduboy, this->ball);
